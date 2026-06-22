dc39d9e378f7a79ee3353b426e6a5f9631c1343b
Abhinav C <abhinavchandrasekar.cs24@bmsce.ac.in>
2026-06-22 15:14:39 +0530

    Time: 0 ms (100%), Space: 8 MB (100%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.c
@@ -0,0 +1,9 @@
+int numWaterBottles(int numBottles, int numExchange) {
+    int sum=numBottles;
+    while (numBottles >= numExchange)
+    {
+        sum+=(numBottles/numExchange);
+        numBottles=((numBottles/numExchange)+(numBottles%numExchange));
+    }
+    return sum;
+}
