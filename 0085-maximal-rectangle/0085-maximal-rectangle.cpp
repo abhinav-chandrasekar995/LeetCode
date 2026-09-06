d424ff1bdfd8aa2bb27cc3c80476a7a47335cc4b
Abhinav C <cabhinav995@gmail.com>
2026-09-06 18:51:29 +0530

    Time: 33 ms (8.06%), Space: 27.6 MB (5.12%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+       vector<int>areas;
+       int rows = matrix.size();
+       int cols = matrix[0].size();
+       for (int i=0; i<rows; i++) {
+        vector<int>histogram;
+        for (int j=0; j<cols; j++) {
+            int mover=i;
+            int height=0;
+            while(mover>=0 && matrix[mover][j]=='1') { 
+                height++;
+                mover--;
+            }
+            histogram.emplace_back(height);
+        }
+        largest_area(histogram,areas);
+       }
+       int ans=-1e9;
+       for(int area: areas) {
+        ans=max(ans,area);
+       }
+       return ans;
+    }
+    void largest_area(vector<int>histogram,vector<int>& areas) {
+        int n=histogram.size();
+        vector<int>pse(n,-1), nse(n,n);
+        stack<int>st;
+
+        for(int i=0;i<n;i++) {
+            while(!(st.empty()) && histogram[i]<=histogram[st.top()]) st.pop();
+            pse[i] = st.empty() ? -1:st.top();
+            st.push(i);
+        }
+
+        while(!(st.empty())) st.pop();
+
+        for(int i=n-1; i>=0; i--) {
+            while(!(st.empty()) && histogram[i]<=histogram[st.top()]) st.pop();
+            nse[i] = st.empty() ? n:st.top();
+            st.push(i);
+        }
+
+        for(int i=0; i<n; i++) {
+            int width=nse[i]-pse[i]-1;
+            int area = histogram[i]*width;
+            int result=max(area,histogram[i]);
+            areas.emplace_back(result);
+        }
+    }
+};
