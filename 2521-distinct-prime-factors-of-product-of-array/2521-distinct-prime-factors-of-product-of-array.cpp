e0ce8ff7c9ded6162c93e7846d089c4cbdec9905
Abhinav C <cabhinav995@gmail.com>
2026-09-14 22:06:29 +0530

    Time: 347 ms (5.08%), Space: 356.4 MB (5.16%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int distinctPrimeFactors(vector<int>& nums) {
+        set<int>store;
+        for(int num: nums) {
+            soe(num, store);
+        }
+        return store.size();
+    }
+    void soe(int num, set<int>& store) {
+        vector<int>verdict(num+1,1);
+        verdict[0]=0;
+        verdict[1]=1;
+        int x=(int)sqrt(num+1);
+        for(int i=2;i<=x;i++) {
+            if(verdict[i]==1) {
+                for(int j=i*i;j<(num+1);j+=i) {
+                    verdict[j]=0;
+                }
+            }
+        }
+        for(int i=2;i<(num+1);i++) {
+            if(verdict[i]==1 && num%i==0) store.insert(i);
+        }
+    }
+};
